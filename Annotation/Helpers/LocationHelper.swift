e455af0c49062cc50672091b9a549803c78bf394
Hayden Hastings <haydenhastings32@gmail.com>
2019-07-11 18:56:39 -0600

    Finished writing the map for the mapViewController

---
+++ Annotation/Helpers/LocationHelper.swift
@@ -0,0 +1,55 @@
+//
+//  LocationHelper.swift
+//  Annotation
+//
+//  Created by Hayden Hastings on 7/11/19.
+//  Copyright © 2019 Hayden Hastings. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationHelper: NSObject, CLLocationManagerDelegate {
+    
+    static var shared: LocationHelper = LocationHelper()
+    
+    lazy var locationManager: CLLocationManager = {
+        let result = CLLocationManager()
+        result.desiredAccuracy = kCLLocationAccuracyKilometer
+        result.pausesLocationUpdatesAutomatically = true
+        result.delegate = self
+        return result
+    }()
+    
+    //Get the users current location
+    var currentLocation: CLLocation? {
+        return locationManager.location
+    }
+    
+    override init() {
+        super.init()
+        requestUserAuthorization()
+    }
+    
+    func requestUserAuthorization() {
+        locationManager.requestAlwaysAuthorization() //added
+        locationManager.requestWhenInUseAuthorization()
+        startTrackingLocation()
+    }
+    
+    func startTrackingLocation() {
+        locationManager.startUpdatingLocation()
+    }
+    
+    func stopTrackingLocation() {
+        locationManager.stopUpdatingLocation()
+    }
+    
+    //CLLocationManagerDelegate method
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let _: CLLocationCoordinate2D = manager.location!.coordinate
+        stopTrackingLocation()
+    }
+}
